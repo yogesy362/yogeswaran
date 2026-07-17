46d2e9c215cc72a3d4a395bc06671c5a2fbf9e2d
yogesy362 <yogesy362@gmail.com>
2026-07-17 11:45:35 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,12 @@
--- Create Course table
-
--- Insert three records
-
--- Display structure
+USE college;
+CREATE TABLE Course(
+CourseID INT PRIMARY KEY,
+Coursename VARCHAR(100),
+Credits INT,
+DepartmentID INT  
+);
+INSERT INTO Course (CourseID,Coursename,Credits,DepartmentID)
+VALUES(101,"Database Management system",4,1),
+(102,"Operating system",3,2),
+(103,"computer Network",4,3);
+DESCRIBE Course;
