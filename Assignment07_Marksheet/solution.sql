d40d2ed8b7588801a2be468e0fd4902afa63f1f9
yogesy362 <yogesy362@gmail.com>
2026-08-27 12:28:26 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -4,4 +4,18 @@
 
 -- Display students with Marks > 80
 
--- Sort by Marks DESC
+-- Sort by Marks DESCCREATE TABLE Marksheet (
+    RollNo INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Department VARCHAR(20),
+    Marks INT
+);
+
+INSERT INTO Marksheet (RollNo, Name, Department, Marks) VALUES
+(1, 'Arun', 'CSE', 55),
+(2, 'Divya', 'IT', 78),
+(3, 'Karthik', 'CSE', 92),
+(4, 'Nisha', 'ECE', 67),
+(5, 'Rahul', 'IT', 88);
+
+SELECT * FROM Marksheet;
