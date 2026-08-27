519ac0a634e2ee40c23b7bc5bf5e2263a2d953fe
yogesy362 <yogesy362@gmail.com>
2026-08-27 12:29:42 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -8,4 +8,19 @@
 
 -- MIN()
 
--- AVG()
+-- AVG()CREATE TABLE Employee (
+	EmployeeID INT PRIMARY KEY,
+	EmployeeName VARCHAR(50),
+	Department VARCHAR(30),
+    Salary INT
+ );
+ 
+ INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary) VALUES
+ (101, 'Ravi', 'HR', 25000),
+ (102, 'Reena', 'IT', 42000),
+ (103, 'Kumar', 'Finance', 35000),
+ (104, 'Suresh', 'IT', 45000),
+ (105, 'Latha', 'HR', 30000);
+ 
+ SELECT COUNT(*) AS Total_Employees 
+ FROM Employee;
